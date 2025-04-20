b14c7511e40b76fd35e68ad132952f3352655003
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-20 21:44:40 +0200

    adding app fonts

---
+++ lib/core/utlis/app_fonts.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task/core/constants/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppFonts{
+  static TextStyle fontSize12Normal = GoogleFonts.oxygen(
+      fontSize: 12,
+      color: AppColors.black,
+      fontWeight: FontWeight.w400
+  );
+  static TextStyle fontSize12Medium = GoogleFonts.oxygen(
+      fontSize: 12,
+      color: AppColors.black,
+      fontWeight: FontWeight.w500
+  );
+  static TextStyle fontSize12SemiBold = GoogleFonts.oxygen(
+      fontSize: 12,
+      color: AppColors.black,
+      fontWeight: FontWeight.w600
+  );
+  static TextStyle fontSize12Bold = GoogleFonts.oxygen(
+      fontSize: 12,
+      color: AppColors.black,
+      fontWeight: FontWeight.w700
+  );
+  static TextStyle fontSize14Normal = GoogleFonts.oxygen(
+      fontSize: 14,
+      color: AppColors.black,
+      fontWeight: FontWeight.w400
+  );
+  static TextStyle fontSize14Medium = GoogleFonts.oxygen(
+      fontSize: 14,
+      color: AppColors.black,
+      fontWeight: FontWeight.w500
+  );
+  static TextStyle fontSize14SemiBold = GoogleFonts.oxygen(
+      fontSize: 14,
+      color: AppColors.black,
+      fontWeight: FontWeight.w600
+  );
+  static TextStyle fontSize14Bold = GoogleFonts.oxygen(
+      fontSize: 14,
+      color: AppColors.black,
+      fontWeight: FontWeight.w700
+  );
+  static TextStyle fontSize16Normal = GoogleFonts.oxygen(
+    fontSize: 16,
+    color: AppColors.black,
+    fontWeight: FontWeight.w400
+  );
+  static TextStyle fontSize16Medium = GoogleFonts.oxygen(
+      fontSize: 16,
+      color: AppColors.black,
+      fontWeight: FontWeight.w500
+  );
+  static TextStyle fontSize16SemiBold = GoogleFonts.oxygen(
+      fontSize: 16,
+      color: AppColors.black,
+      fontWeight: FontWeight.w600
+  );
+  static TextStyle fontSize16Bold = GoogleFonts.oxygen(
+      fontSize: 16,
+      color: AppColors.black,
+      fontWeight: FontWeight.w700
+  );
+  static TextStyle fontSize18Normal = GoogleFonts.oxygen(
+      fontSize: 18,
+      color: AppColors.black,
+      fontWeight: FontWeight.w400
+  );
+  static TextStyle fontSize18Medium = GoogleFonts.oxygen(
+      fontSize: 18,
+      color: AppColors.black,
+      fontWeight: FontWeight.w500
+  );
+  static TextStyle fontSize18SemiBold = GoogleFonts.oxygen(
+      fontSize: 18,
+      color: AppColors.black,
+      fontWeight: FontWeight.w600
+  );
+  static TextStyle fontSize18Bold = GoogleFonts.oxygen(
+      fontSize: 18,
+      color: AppColors.black,
+      fontWeight: FontWeight.w700
+  );
+  static TextStyle fontSize20Bold = GoogleFonts.oxygen(
+      fontSize: 20,
+      color: AppColors.black,
+      fontWeight: FontWeight.w700
+  );
+  static TextStyle fontSize24Bold = GoogleFonts.oxygen(
+      fontSize: 24,
+      color: AppColors.black,
+      fontWeight: FontWeight.w700
+  );
+
+}
