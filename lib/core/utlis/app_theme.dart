709b50290bb5f3007418a0651b56b0f39df29e25
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-20 15:22:00 +0200

    adding app theme

---
+++ lib/core/utlis/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task/core/constants/app_colors.dart';
+
+abstract class AppTheme{
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: AppColors.black,
+  );
+}
