bfa35266b313e30c3ffd87aacc76830d370281a9
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-20 22:44:40 +0200

    adding shopping bag image

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,4 +1,5 @@
 abstract class AppImages{
   static const String imageNotAvailable = "assets/images/Image_not_available.png";
   static const String emptyAnimation = "assets/images/empty_animation.json";
+  static const String shoppingBag = "assets/images/shopping_bag.png";
 }
