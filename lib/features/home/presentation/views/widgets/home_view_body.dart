559ef9d82eb9136a93fa0bf0ad52e669418728d0
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-20 21:48:17 +0200

    adding initial home design and logic

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task/features/home/presentation/views/widgets/home_sliver_app_bar.dart';
+import 'package:flutter_task/features/home/presentation/views/widgets/products_grid_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-
-      ],
+    return NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          const HomeSliverAppBar(),
+        ],
+        body: const ProductsGridView()
     );
   }
 }
