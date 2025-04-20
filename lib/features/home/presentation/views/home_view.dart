bcb0bcae07f216f1854e180d82e1bc7b4c1c4ac2
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-20 21:47:52 +0200

    providing home cubit

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_task/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:flutter_task/features/home/presentation/views_model/home_cubit.dart';
 
 class HomeView extends StatelessWidget {
-  static String routeName = "HomeView";
+  static const String routeName = "HomeView";
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeViewBody(),
+    return Scaffold(
+      body: BlocProvider<HomeCubit>(
+        create: (context) => HomeCubit(),
+        child: const SafeArea(
+            child: HomeViewBody()
+        ),
+      ),
     );
   }
 }
