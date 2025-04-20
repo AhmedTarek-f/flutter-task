ea6c526e929e8a469b545f3420a4b76f73a39761
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-20 21:45:01 +0200

    adding product details view to app router

--- lib/core/utlis/app_router.dart
+++ lib/core/utlis/app_router.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task/features/home/presentation/views/home_view.dart';
+import 'package:flutter_task/features/product_details/presentation/views/product_details_view.dart';
 
 abstract class AppRouter{
   static Map<String, Widget Function(BuildContext)> routes = {
     HomeView.routeName: (_) => const HomeView(),
+    ProductDetailsView.routeName: (_) => const ProductDetailsView()
   };
 }
