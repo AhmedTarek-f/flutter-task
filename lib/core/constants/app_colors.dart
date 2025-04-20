e3fa59f52c0bee72054d661aa17db0a934b8d5f6
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-20 21:41:51 +0200

    modified app colors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 abstract final class AppColors{
-  static const Color primary = Color(0xffE2BE7F);
-  static const Color black = Color(0xff212121);
+  static const Color primary = Color(0xff704F38);
+  static const Color black = Color(0xff000000);
   static const Color white = Color(0xffFFFFFF);
   static const Color offWhite = Color(0xffFEFFE8);
-  static const Color gray = Color(0xff707070);
+  static const Color gray = Color(0xff9E9E9E);
   static const Color brown = Color(0xff946107);
   static const Color red = Color(0xffFE4039);
-  static const Color orange = Color(0xffFFAD3B);
+  static const Color gold = Color(0xffFCAF23);
 }
