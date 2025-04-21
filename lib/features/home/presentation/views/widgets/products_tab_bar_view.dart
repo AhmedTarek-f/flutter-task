df1672086b2b19512782f71fea6a84080540788d
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-21 12:51:26 +0200

    make the device orientation vertically

--- lib/features/home/presentation/views/widgets/products_tab_bar_view.dart
+++ lib/features/home/presentation/views/widgets/products_tab_bar_view.dart
@@ -4,12 +4,12 @@ import 'package:flutter_task/core/common_widgets/loading_indicator.dart';
 import 'package:flutter_task/core/constants/app_images.dart';
 import 'package:flutter_task/core/utlis/loaders/animation_loader.dart';
 import 'package:flutter_task/core/utlis/loaders/loaders.dart';
-import 'package:flutter_task/features/home/presentation/views/widgets/product_item.dart';
+import 'package:flutter_task/features/home/presentation/views/widgets/products_grid_view.dart';
 import 'package:flutter_task/features/home/presentation/views_model/home_cubit.dart';
 import 'package:flutter_task/features/home/presentation/views_model/home_state.dart';
 
-class ProductsGridView extends StatelessWidget {
-  const ProductsGridView({
+class ProductsTabView extends StatelessWidget {
+  const ProductsTabView({
     super.key,
   });
 
@@ -28,16 +28,7 @@ class ProductsGridView extends StatelessWidget {
         const LoadingIndicator():
         controller.productsList.isEmpty?
         const AnimationLoaderWidget(text: "There are no available products right now.", animation: AppImages.emptyAnimation):
-        GridView.builder(
-        physics: const BouncingScrollPhysics(),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            mainAxisSpacing: 8,
-            childAspectRatio: 1/1.05
-          ),
-        itemBuilder: (context, index) => ProductItem(productData: controller.productsList[index],),
-        itemCount: controller.productsList.length,
-      ),
+        ProductsGridView(productsList: controller.productsList,)
     );
   }
 }
