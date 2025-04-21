313ec6d9f7996fc76e3bb1c46e216d0c6796c7af
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-21 12:57:19 +0200

    adding search icon

--- lib/features/home/presentation/views/widgets/home_sliver_app_bar.dart
+++ lib/features/home/presentation/views/widgets/home_sliver_app_bar.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_task/core/constants/app_colors.dart';
+import 'package:flutter_task/core/constants/app_images.dart';
 import 'package:flutter_task/core/utlis/app_fonts.dart';
+import 'package:flutter_task/features/home/presentation/views_model/home_cubit.dart';
 
 class HomeSliverAppBar extends StatelessWidget {
   const HomeSliverAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = BlocProvider.of<HomeCubit>(context);
     return SliverAppBar(
       backgroundColor: Colors.transparent,
       flexibleSpace: FlexibleSpaceBar(
         title: Text(
           "MrPerfection Store",
           style: AppFonts.fontSize24Bold.copyWith(color: AppColors.primary),
+        ),
+        background: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            RPadding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: IconButton(
+                  onPressed: (){
+                    controller.toggleSearchField();
+                  },
+                  icon: ImageIcon(
+                      const AssetImage(AppImages.searchIcon),
+                    size: 28.r,
+                    color: AppColors.primary,
+                  )
+              ),
+            ),
+          ],
         ),
         centerTitle: true,
       ),
