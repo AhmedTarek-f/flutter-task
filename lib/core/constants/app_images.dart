e33995164481231d8c8e8b203fe8b8664d387292
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-21 12:52:22 +0200

    adding search icon

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -2,4 +2,5 @@ abstract class AppImages{
   static const String imageNotAvailable = "assets/images/Image_not_available.png";
   static const String emptyAnimation = "assets/images/empty_animation.json";
   static const String shoppingBag = "assets/images/shopping_bag.png";
+  static const String searchIcon = "assets/images/search.png";
 }
